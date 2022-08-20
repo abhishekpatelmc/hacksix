1b9f0eaa5254a7224bec784ae98687c065ae2514
hackerrank <73036580+Rajpreetgill@users.noreply.github.com>
2022-08-19 22:51:18 -0400

    added rectangles for landing page along with page title and description
    Signed-off-by: hackerrank <73036580+Rajpreetgill@users.noreply.github.com>

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+import './HomePage.css';
 
 function HomePage() {
   return (
-    <div>HomePage</div>
+    <div class ="homePage">
+
+      <div class = "leftRectangle">
+        
+          <div class = "pageTitle">
+            <p>EZFinance</p>
+          </div>
+
+          <div class = "pageDescription">
+            <p>Your one stop website for all financial advice! 
+              Create an account or sign in to get started!</p>
+          </div>
+
+      </div>
+
+      <div class = "rightRectangle">
+      </div>
+
+    </div>
   )
 }
 
-export default HomePage
+export default HomePage;
