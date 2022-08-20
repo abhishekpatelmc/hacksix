3dde0a80a177c4c3b1d4f268a242ad39d7c2b0db
Abhishek patel <mcabhishekpatel2017@gmail.com>
2022-08-20 03:10:21 -0400

    Added Navbar and landing page -- not final

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import "./Homepage.css";
+import imagelogo from "../../asset/img/undraw_savings_re_eq4w.svg";
 
 function HomePage() {
   return (
-    <div>HomePage</div>
-  )
+    <div>
+      <Navbar />
+      <div className="landingContainer">
+        <div className="landingInfo">
+          <h1>A NEW WAY TO LEARN fINANCE</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
+            odit nihil ullam nesciunt quidem iste, Repellendus odit nihil
+          </p>
+          <button>Get Started</button>
+        </div>
+        <div className="landingImg">
+          <img src={imagelogo} alt="LandingImage" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default HomePage
+export default HomePage;
