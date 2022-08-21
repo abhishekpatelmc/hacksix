ac7d2eeef484afa21161bec562bff6ffe2d02446
Abhishek patel <mcabhishekpatel2017@gmail.com>
2022-08-21 01:06:10 -0400

    scroll bar activated -- buttons still not working

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,10 +1,23 @@
 import * as React from "react";
+import aboutImg from "../../asset/img/undraw_printing_invoices_-5-r4r.svg";
 import "./About.css";
 
 const About = () => {
   return (
-    <div className="Heading">
-      <h1> About </h1>
+    <div>
+      <div className="Heading">
+        <h1> About </h1>
+      </div>
+      <div className="MainContainer">
+        <div className="Image">
+          <img src={aboutImg} alt="about" />
+        </div>
+        <div className="Info">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
