112396bc0feb62a97e08915494922bb3035bf1f4
Abhishek patel <mcabhishekpatel2017@gmail.com>
2022-08-21 02:10:08 -0400

    final commit

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -13,8 +13,16 @@ const About = () => {
           <img src={aboutImg} alt="about" />
         </div>
         <div className="Info">
+          <h1>
+            A NEW WAY TO LEARN <br />
+            FINANCE
+          </h1>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+            EZFinance gathers all financial resources in one place and is
+            completely free for users to use. It offers tailored information for
+            newcomers and students to help leverage their financial freedom
+            while ensuring marginalized communities across Canada have full
+            access to financial information from a single source.
           </p>
         </div>
       </div>
